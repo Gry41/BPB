19691c266e852e6ceb7b930d72a86a7eb7fd88fc
Greidy <greidy.valdes@gmail.com>
2018-07-01 17:28:40 -0400

    first models
    a borrar

---
+++ BPB/test/system/idiomas_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class IdiomasTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit idiomas_url
+  #
+  #   assert_selector "h1", text: "Idioma"
+  # end
+end
