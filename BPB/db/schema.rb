13c8ddad0ffbbd0d9c4b9412912558cb494c2831
Greidy <greidy.valdes@gmail.com>
2018-07-01 17:59:28 -0400

    language services
    models

--- BPB/db/schema.rb
+++ BPB/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180701214915) do
+ActiveRecord::Schema.define(version: 20180701215738) do
 
   create_table "facilities", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 20180701214915) do
   end
 
   create_table "idiomas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
